82e4cc35f7c509addf4fa3bff608d5a3fd09efa3
tirumalasettyvk98 <tirumalasettyvk98@gmail.com>
2021-11-09 12:13:06 +0530

    added test and hai controller

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -6,10 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+	
+	
+	@GetMapping("/")
+	public String hello()
+	{
+		return "welcome to jenkins";
+	}
+	
 	@GetMapping("/first")
 	public String first()
 	{
 		return "first service hai war vamsi from git pushing";
 	}
+	
+	
 	
 }
--- src/test/java/com/example/demo/WarProjectApplicationTests.java
+++ src/test/java/com/example/demo/WarProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,17 @@ class WarProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertEquals(2, 2);
 	}
+	
+	
+	@Test
+	void Test1() {
+		
+		assertEquals("vamsi","vamsi");
+	}
+	
+	
 
 }
