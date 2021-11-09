4396c9d3fd5e3d91f8e77f02dc5794be72a1aca1
tirumalasettyvk98 <tirumalasettyvk98@gmail.com>
2021-11-09 07:24:48 +0530

    first commit on war project

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/first")
+	public String first()
+	{
+		return "first service hai war ";
+	}
+	
+}
